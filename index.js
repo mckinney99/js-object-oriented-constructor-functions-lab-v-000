4c76198509364286cf48c994d0d43d925b48defc
Eric McKinney <mckinney99@gmail.com>
2018-02-27 16:31:52 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,17 @@
+function Scooter(){
+  this.year
+  this.color
+  this.model
+  this.properties
+}
+
+function Driver(){
+  this.name
+  this.age
+  this.experience
+}
+
+function PickupLocation(){
+  this.address
+  this.city
+}
