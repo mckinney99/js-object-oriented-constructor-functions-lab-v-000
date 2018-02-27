5cbad2578c882d08695c78d89c0c40c85e1c42cc
Eric McKinney <mckinney99@gmail.com>
2018-02-27 16:34:03 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,17 @@
+function Scooter(year, color, model, properties){
+  this.year = year
+  this.color = color
+  this.model = model
+  this.properties = properties
+}
+
+function Driver(name, age, experience){
+  this.name = name
+  this.age = age
+  this.experience = experience
+}
+
+function PickupLocation(address, city){
+  this.address = address
+  this.city = city
+}
